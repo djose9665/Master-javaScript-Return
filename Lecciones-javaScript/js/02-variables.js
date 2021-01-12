14c2dec9d2d024ff32c60fe07c30c94fedf22d28
Jose Daniel <djose9665@gmail.com>
2021-01-12 08:09:24 -0600

    leccion de variables

---
+++ Lecciones-javaScript/js/02-variables.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var cancionActual = "You give love a bad name";
+
+alert("Primera cancion: " + cancionActual);
+
+cancionActual = "Should I stay or should I go";
+
+console.log("Nueva cancion: " + cancionActual);
