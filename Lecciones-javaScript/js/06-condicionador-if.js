d1195e33bac4407c49d0863958610893bafbb110
Jose Daniel <djose9665@gmail.com>
2021-01-15 08:24:27 -0600

    Leccion condiciones y operadores logicos

--- Lecciones-javaScript/js/06-condicionador-if.js
+++ Lecciones-javaScript/js/06-condicionador-if.js
@@ -11,3 +11,30 @@ if(edad < 18){
 else{
   alert("Tienes " + String(edad) +  " bienvenido");
 }
+
+
+// Condicional IF con operadores logicos
+
+var velocidad = 25;
+
+if(velocidad >= 40 && velocidad <= 60){
+  alert("Tu velocidad en buena");
+}
+else if(velocidad < 40 && velocidad >= 10 ){
+  alert("Tu velocidad es baja");
+}
+else if( velocidad > 60 && velocidad <= 120){
+  alert("Vas a alta velocidad");
+}
+else if(velocidad > 120 ){
+  alert("Cuidado!! Disminuye tu velocidad");
+}
+else if(velocidad == 0){
+  alert("Enciende el auto");
+}
+else if(velocidad < 4 || velocidad < 10){
+  alert("Puedes aumentar la velocidad")
+}
+else{
+  alert("Error");
+}
