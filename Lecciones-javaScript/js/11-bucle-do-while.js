81f3a2aa83800dfac0129c8d8263dabd876bc5e9
Jose Daniel <djose9665@gmail.com>
2021-01-19 06:39:36 -0600

    Bucle Do While

---
+++ Lecciones-javaScript/js/11-bucle-do-while.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var leccion = 29;
+
+do {
+  console.log("Estas haciendo la leccion: " + leccion);
+  leccion ++;
+} while (leccion <= 210);
+
+if(leccion = 210){
+  alert("Felicidades!!  Terminaste el curso");
+}
