e82e30124cd99db45c9516b3dac3caba45717258
Jose Daniel <djose9665@gmail.com>
2021-01-22 07:49:08 -0600

    Leccion 13, funciones

---
+++ Lecciones-javaScript/js/21-funciones.js
@@ -0,0 +1,16 @@
+'use strict'
+// Funciones
+  /**
+   * Las funciones son un grupo de ordenes, reglas o instrucciones definidas por un nombre en concreto 
+   * que se pueden utilizar indefinidamente 
+   */
+
+function despertar(){
+  console.log("Ya es hora de despertar");
+  console.log("Son las 6 AM");
+
+  // return "Funcion";
+}
+
+// Asi se manda llamar una funcion
+despertar();
