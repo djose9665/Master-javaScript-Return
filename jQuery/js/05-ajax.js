7adf7665fafc0ce978e91dfd55176a155a17a971
Jose Daniel <djose9665@gmail.com>
2021-03-05 08:07:28 -0600

    Leccion 30, Post jQuery

--- jQuery/js/05-ajax.js
+++ jQuery/js/05-ajax.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 
   var datos = $('#div_datos');
+  var formulario = $('#form_registro');
  
   // Load
   // datos.load("https://reqres.in/"); 
 
   // Get y Post
-  $.get("https://reqres.in/api/users?", {page: 2}, function(response){
+  $.get("https://reqres.in/api/users", {page: 2}, function(response){
     response.data.forEach((element, index)=>{
       datos.append("<p>" + index + " " + element.first_name + " " + element.last_name + "</p>");
     })
@@ -17,6 +18,31 @@ $(document).ready(function(){
     response.data.forEach((element, index)=>{
       datos.append("<p>" + index + " " + element.name + "</p>");
     });
+  });
+
+  formulario.submit(function(e){
+    e.preventDefault();
+
+    var usuario = {
+      // name : "Daniel Martinez",
+      // web : "djjson.online",
+      // email : "djjso0n@gmail.com"
+      // name: $('#txt_nombre').val(),
+      // web: $('#txt_web').val(),
+      // email: $('#txt_email').val()
+      name: $('input[name="txt_nombre"]').val(),
+      web: $('input[name="txt_web"]').val(),
+      email: $('input[name="txt_email"]').val()
+    };
+    console.log(usuario);
+  
+    // $.post("https://reqres.in/api/users", usuario, function(response){
+      $.post($(this).attr("action"), usuario, function(response){
+      console.log(response);
+    }).done(function(){
+      alert("Usuario registrado correctamente");
+    });
+    return false;
   });
 
 });
