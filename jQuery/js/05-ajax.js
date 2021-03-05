6391cb515bbc569e8521d3f903e32e9913ae6db4
Jose Daniel <djose9665@gmail.com>
2021-03-05 07:27:17 -0600

    Leccion 30, Peticion Get jQuery

--- jQuery/js/05-ajax.js
+++ jQuery/js/05-ajax.js
@@ -3,6 +3,20 @@ $(document).ready(function(){
   var datos = $('#div_datos');
  
   // Load
-  datos.load("https://reqres.in/"); 
+  // datos.load("https://reqres.in/"); 
+
+  // Get y Post
+  $.get("https://reqres.in/api/users?", {page: 2}, function(response){
+    response.data.forEach((element, index)=>{
+      datos.append("<p>" + index + " " + element.first_name + " " + element.last_name + "</p>");
+    })
+  });
+
+  $.get("https://reqres.in/api/unknown",{page: 2}, function(response){
+    console.log(response);
+    response.data.forEach((element, index)=>{
+      datos.append("<p>" + index + " " + element.name + "</p>");
+    });
+  });
 
 });
