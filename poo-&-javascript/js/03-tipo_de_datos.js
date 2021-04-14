c84e93f78c0bfa0f4ee5c1544ccdaad9af73bd83
Jose Daniel <djose9665@gmail.com>
2021-04-14 08:12:23 -0500

    Leccion 36, Primeros pasos con TypeScript

---
+++ poo-&-javascript/js/03-tipo_de_datos.js
@@ -0,0 +1,14 @@
+// String 
+var cadena = "Daniel Martinez";
+// Number
+var edad = 45;
+// boleano
+var verdadero_falso = true;
+// Any
+var cualquiera = "Hola";
+cualquiera = "Nuevo";
+// arrays (<string, number, any,boolean >)
+var lenguajes = ["JavaScript", "PHP", "JAVA"];
+// var lenguajes: Array<boolean> = [true, false];
+var cancion = ["This is a war", "La vuelta al mundo", "Box"];
+console.log(cadena, edad, verdadero_falso, cualquiera, lenguajes, cancion);
