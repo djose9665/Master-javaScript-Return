e42a380bd5ca4f6330796e183703837e9c96e4bf
Jose Daniel <djose9665@gmail.com>
2021-04-26 08:01:21 -0500

    Leccion 36, Constructores, Interfaces y Herencia

--- poo-&-javascript/js/05-clases.js
+++ poo-&-javascript/js/05-clases.js
@@ -1,12 +1,32 @@
 'use strict';
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 window.addEventListener('load', function () {
-    console.log("Clase");
+    // console.log("Clase");
     // Clase(Molde del objeto)
     // Propiedades (Caracteristicas del objeto)
     // Metodos (funciones o acciones del objeto)
     // Clase de una camiseta
     var Clases = /** @class */ (function () {
-        function Clases() {
+        function Clases(color, modelo, marca, talla, precio) {
+            this.color = color;
+            this.modelo = modelo;
+            this.marca = marca;
+            this.talla = talla;
+            this.precio = precio;
         }
         Clases.prototype.setColor = function (nuevo_color) {
             this.color = nuevo_color;
@@ -16,18 +36,23 @@ window.addEventListener('load', function () {
         };
         return Clases;
     }());
-    var camiseta = new Clases();
-    camiseta.setColor("Verde");
-    // camiseta.color = "Negro";
-    // camiseta.modelo = "Manga corta";
-    // camiseta.marca = "Adidas";
-    // camiseta.talla = "S";
-    // camiseta.precio = 199;
-    // var playera = new Clases();
-    // playera.color = "Roja";
-    // playera.modelo = "Manga larga";
-    // playera.marca = "Nike";
-    // playera.talla = "S";
-    // playera.precio = 250;
+    var Sudadera = /** @class */ (function (_super) {
+        __extends(Sudadera, _super);
+        function Sudadera() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Sudadera.prototype.setCapucha = function (capucha) {
+            this.capucha = capucha;
+        };
+        Sudadera.prototype.getCapucha = function () {
+            return this.capucha;
+        };
+        return Sudadera;
+    }(Clases));
+    var sudadera_nike = new Sudadera("gris", "Manga Larga", "Nike", "L", 400);
+    sudadera_nike.setCapucha(true);
+    sudadera_nike.setColor("Negro");
+    console.log(sudadera_nike);
+    var camiseta = new Clases("Rojo", "Manga larga", "Nike", "L", 250);
     console.log(camiseta);
 });
--- poo-&-javascript/js/05-clases.ts
+++ poo-&-javascript/js/05-clases.ts
@@ -1,13 +1,19 @@
 'use strict'
 window.addEventListener('load', function(){
-  console.log("Clase");
+  // console.log("Clase");
+
+  // Interface:
+  interface CamisetaBase{
+    setColor(color);
+    getColor();
+  }
 
   // Clase(Molde del objeto)
   // Propiedades (Caracteristicas del objeto)
   // Metodos (funciones o acciones del objeto)
 
   // Clase de una camiseta
-  class Clases{
+  class Clases implements CamisetaBase{
     // Propiedades
     private color: string;
     private modelo: string;
@@ -15,6 +21,14 @@ window.addEventListener('load', function(){
     private talla: string;
     private precio: number;
 
+    constructor(color, modelo, marca, talla, precio){
+      this.color = color;
+      this.modelo = modelo;
+      this.marca = marca;
+      this.talla = talla;
+      this.precio = precio;
+    }
+    
     public setColor(nuevo_color){
       this.color = nuevo_color; 
     }
@@ -25,22 +39,23 @@ window.addEventListener('load', function(){
 
   }
 
-  var camiseta = new Clases();
-  camiseta.setColor("Verde");
-  // camiseta.color = "Negro";
-  // camiseta.modelo = "Manga corta";
-  // camiseta.marca = "Adidas";
-  // camiseta.talla = "S";
-  // camiseta.precio = 199;
+  class Sudadera extends Clases{
+    public capucha: boolean;
 
-  // var playera = new Clases();
+    setCapucha(capucha: boolean){
+      this.capucha = capucha;
+    }
+    getCapucha():boolean{
+      return this.capucha;
+    }
+  }
 
-  // playera.color = "Roja";
-  // playera.modelo = "Manga larga";
-  // playera.marca = "Nike";
-  // playera.talla = "S";
-  // playera.precio = 250;
+  var sudadera_nike = new Sudadera("gris", "Manga Larga", "Nike", "L", 400);
+  sudadera_nike.setCapucha(true);
+  sudadera_nike.setColor("Negro");
+  console.log(sudadera_nike);
 
-  console.log(camiseta,  );
+  var camiseta = new Clases("Rojo", "Manga larga", "Nike", "L", 250);
+  console.log(camiseta);
     
 });
