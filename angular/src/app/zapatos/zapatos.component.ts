9139928304988afb011a57d6abe5f58ff5ea106c
Jose Daniel <djose9665@gmail.com>
2021-05-20 08:34:27 -0500

    Leccion 37, Directivas

--- angular/src/app/zapatos/zapatos.component.ts
+++ angular/src/app/zapatos/zapatos.component.ts
@@ -10,17 +10,33 @@ export class ZapatosComponent implements OnInit{
   // Propiedades
   title: string;
   public zapatos: Array<Zapato>;
+  public marcas: String[];
 
   constructor(){
+    this.marcas = new Array();
     this.title = "Componente de zapatos";
 
     this.zapatos = [
       new Zapato('Nike Classic', 'Nike', 'Negro', 50, true),
+      new Zapato('Nike Classic', 'Nike', 'Negro', 50, true),
+      new Zapato('Adidas Yezzy', 'Adidas', 'Blanco', 120, false),
       new Zapato('Adidas Yezzy', 'Adidas', 'Blanco', 120, true),
     ];
   }
 
   ngOnInit(){
     console.log(this.zapatos);
+
+    this.getMarcas();
+  }
+
+  getMarcas(){
+    this.zapatos.forEach((zapato, index)=>{
+      if(this.marcas.indexOf(zapato.marca) < 0){
+        this.marcas.push(zapato.marca);
+      }
+    });
+
+    console.log(this.marcas)
   }
 }
